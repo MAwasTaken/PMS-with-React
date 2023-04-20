0fcc20d2dd4f15d987359de585ee3dd3b153c18d
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-04-20 13:39:47 +0330

    Sidebar Icons Created

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -4,9 +4,14 @@ import React from "react";
 // packages
 
 // styles
-import './Sidebar.css'
+import "./Sidebar.css";
 
 // components
+import { AiOutlineHome } from "react-icons/ai";
+import { MdProductionQuantityLimits } from "react-icons/md";
+import { BiCommentDetail, BiDollar } from "react-icons/bi";
+import { FiUsers } from "react-icons/fi";
+import { BsBagCheck } from "react-icons/bs";
 
 // sidebar
 function Sidebar() {
@@ -15,22 +20,40 @@ function Sidebar() {
 			<h1 className='sidebar-title'>به داشبورد خود خوش آمدید</h1>
 			<ul className='sidebar-links'>
 				<li>
-					<a href='#'>صفحه اصلی</a>
+					<a href='#'>
+						<AiOutlineHome className='icon' />
+						صفحه اصلی
+					</a>
 				</li>
-				<li className="active">
-					<a href='#'>محصولات</a>
-				</li>
-				<li>
-					<a href='#'>کامنت ها</a>
-				</li>
-				<li>
-					<a href='#'>کاربران</a>
-				</li>
-				<li>
-					<a href='#'>سفارشات</a>
+				<li className='active'>
+					<a href='#'>
+						<MdProductionQuantityLimits className='icon' />
+						محصولات
+					</a>
 				</li>
 				<li>
-					<a href='#'>تخفیف ها</a>
+					<a href='#'>
+						<BiCommentDetail className='icon' />
+						کامنت ها
+					</a>
+				</li>
+				<li>
+					<a href='#'>
+						<FiUsers className='icon' />
+						کاربران
+					</a>
+				</li>
+				<li>
+					<a href='#'>
+						<BsBagCheck className='icon' />
+						سفارشات
+					</a>
+				</li>
+				<li>
+					<a href='#'>
+						<BiDollar className='icon' />
+						تخفیف ها
+					</a>
 				</li>
 			</ul>
 		</div>
