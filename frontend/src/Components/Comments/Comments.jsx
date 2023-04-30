365a47451b0c25a3ee6c621432c99f974b7adea5
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-04-30 11:09:45 +0330

    backend add to project

--- frontend/src/Components/Comments/Comments.jsx
+++ frontend/src/Components/Comments/Comments.jsx
@@ -7,14 +7,12 @@ import React from "react";
 
 // components
 import Errorbox from "../Errorbox/Errorbox";
-import DeleteModal from "../DeleteModal/DeleteModal";
 
 // comments
 function Comments() {
 	return (
 		<>
 			<Errorbox msg='هیچ کامنتی یافت نشد!' />
-			<DeleteModal />
 		</>
 	);
 }
