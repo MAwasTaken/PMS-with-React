431a45afc8ac5a30d7022ccf8b2881eb881fa9ec
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-05-15 11:47:25 +0330

    Users Created

--- frontend/src/Components/Users/Users.jsx
+++ frontend/src/Components/Users/Users.jsx
@@ -1,18 +1,59 @@
 // react
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // packages
 
 // styles
+import "./Users.css";
 
 // components
 import Errorbox from "../Errorbox/Errorbox";
 
 // users
 function Users() {
+	// states
+	const [users, setUsers] = useState([]);
+
+	// side effects
+	useEffect(() => {
+		fetch(`http://localhost:3000/api/users`)
+			.then((res) => res.json())
+			.then((users) => setUsers(users));
+	}, []);
+
 	return (
 		<div>
-			<Errorbox msg='هیچ کاربری یافت نشد!'/>
+			{users.length ? (
+				<div className='cms-main'>
+					<h1 className='cms-title'>لیست کاربران</h1>
+					<table className='cms-table'>
+						<thead>
+							<tr>
+								<th>نام و نام خانوادگی</th>
+								<th>نام کاربری</th>
+								<th>رمز عبور</th>
+								<th>شماره تماس</th>
+								<th>ایمیل</th>
+							</tr>
+						</thead>
+						<tbody>
+							{users.map((user) => (
+								<tr key={user.id}>
+									<td>
+										{user.firsname} {user.lastname}
+									</td>
+									<td>{user.username}</td>
+									<td>{user.password}</td>
+									<td>{user.phone}</td>
+									<td>{user.email}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			) : (
+				<Errorbox msg='هیچ کاربری یافت نشد!' />
+			)}
 		</div>
 	);
 }
